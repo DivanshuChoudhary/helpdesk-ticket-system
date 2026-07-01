1a0db4bb77e471ce0ad363dd99fce8d991874f07
DivanshuChoudhary <choudharydivanshu75@gmail.com>
2026-07-01 18:33:54 +0530

    add dummy ticket id and get all ticket API

--- index.js
+++ index.js
@@ -4,6 +4,30 @@ const app = express();
 
 const PORT = 3000;
 
+const tickets = [
+    {
+        id: 1,
+        title: "Internet not working",
+        description: "Unable to connect to office WiFi",
+        status: "Open",
+        priority: "High"
+    },
+    {
+        id: 2,
+        title: "Printer issue",
+        description: "Printer is not printing documents",
+        status: "In Progress",
+        priority: "Medium"
+    },
+    {
+        id: 3,
+        title: "Email login problem",
+        description: "Forgot email password",
+        status: "Closed",
+        priority: "Low"
+    }
+];
+
 app.get("/", (req, res) => {
     res.send("Welcome to HelpDesk Ticket System API 🚀");
 });
@@ -14,6 +38,10 @@ app.get("/about", (req, res) => {
 
 app.get("/health", (req, res) => {
     res.send("Server is running successfully ✅");
+});
+
+app.get("/tickets", (req, res) => {
+    res.json(tickets);
 });
 
 app.listen(PORT, () => {
