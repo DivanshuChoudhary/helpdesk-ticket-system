d4ff64a6dec3dccea40635778c9a45b4f6bca883
DivanshuChoudhary <choudharydivanshu75@gmail.com>
2026-07-01 18:31:09 +0530

    add basic application routes

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get("/", (req, res) => {
     res.send("Welcome to HelpDesk Ticket System API 🚀");
 });
 
+app.get("/about", (req, res) => {
+    res.send("HelpDesk Ticket System API Version 1.0");
+});
+
+app.get("/health", (req, res) => {
+    res.send("Server is running successfully ✅");
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
