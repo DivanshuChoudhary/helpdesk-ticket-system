39a408e82b1800f72a454f49f4722a7826c670a0
DivanshuChoudhary <choudharydivanshu75@gmail.com>
2026-07-02 19:02:07 +0530

    dashboard card

--- public/script.js
+++ public/script.js
@@ -14,6 +14,11 @@ async function loadTickets() {
                     <td>${ticket.title}</td>
                     <td>${ticket.status}</td>
                     <td>${ticket.priority}</td>
+                    <td>
+                        <button onclick="deleteTicket(${ticket.id})">
+                            Delete
+                        </button>
+                    </td>
                 </tr>
             `;
         });
@@ -23,29 +28,23 @@ async function loadTickets() {
     }
 }
 
-const ticketTable = document.getElementById("ticketTable");
+async function deleteTicket(id) {
 
-async function loadTickets() {
+    const confirmDelete = confirm("Are you sure you want to delete this ticket?");
+
+    if (!confirmDelete) return;
+
     try {
-        const response = await fetch("/tickets");
-        const tickets = await response.json();
 
-        ticketTable.innerHTML = "";
-
-        tickets.forEach((ticket) => {
-            ticketTable.innerHTML += `
-                <tr>
-                    <td>${ticket.id}</td>
-                    <td>${ticket.title}</td>
-                    <td>${ticket.status}</td>
-                    <td>${ticket.priority}</td>
-                    <td>
-                        <button class="edit-btn">Edit</button>
-                        <button class="delete-btn">Delete</button>
-                    </td>
-                </tr>
-            `;
+        const response = await fetch(`/tickets/${id}`, {
+            method: "DELETE"
         });
+
+        const data = await response.json();
+
+        alert(data.message);
+
+        loadTickets();
 
     } catch (error) {
         console.error(error);
